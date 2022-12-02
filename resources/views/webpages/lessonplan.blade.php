55e606ff083bc4ed79e6aa6a5c8622e9dfb2ce30
rahul rajput <rahulkumar.1504@hotmail.com>
2022-12-02 23:40:51 +0530

    vimeo player bug fixed

--- resources/views/webpages/lessonplan.blade.php
+++ resources/views/webpages/lessonplan.blade.php
@@ -17,6 +17,26 @@
                 <div class="row">
                     @if ($lessonPlan->first())
                         @foreach ($lessonPlan as $cdata)
+                            @php
+                                $parsed = parse_url($cdata->video_url);
+                                $checkUrlHost = explode('.', $parsed['host']);
+                                if (in_array('youtube', $checkUrlHost)) {
+                                    $video_id = explode('?v=', $cdata->video_url);
+                                    if (empty($video_id[1])) {
+                                        $video_id = explode('/v/', $cdata->video_url);
+                                    }
+                                    $video_id = explode('&', $video_id[1]);
+                                    $video_id = $video_id[0];
+                                    $video_url = 'https://www.youtube.com/embed/' . $video_id;
+                                } elseif (in_array('vimeo', $checkUrlHost)) {
+                                    $parse_vimeo = parse_url($cdata->video_url, PHP_URL_PATH);
+                                    $video_id = array_values(array_filter(explode('/', $parse_vimeo)));
+                                    $para_vimeo = count($video_id) > 1 ? '?h=' . $video_id[1] : '';
+                                    $video_url = 'https://player.vimeo.com/video/' . $video_id[0] . $para_vimeo;
+                                } else {
+                                    $video_url = '';
+                                }
+                            @endphp
                             <div class="col-xl-4 col-md-6 col-12 px-4">
                                 <div class="card">
                                     <img class=""
@@ -45,55 +65,17 @@
                                             @endif
 
                                             @if ($cdata->video_url)
-                                                <button type="button" class="btn btn-warning btn-sm" data-bs-toggle="modal"
-                                                    data-bs-target="#bs-video-modal-{{ $cdata->id }}">Instructions Video</button>
+                                                <button id="vid-btn-{{ $cdata->id }}" data-video="{{ $video_url }}"
+                                                    type="button" class="play-video btn btn-warning btn-sm"
+                                                    data-bs-toggle="modal" data-bs-target="#bs-video-modal">Instructions
+                                                    Video</button>
+                                                {{-- <a class="popup-youtube btn btn-primary" href="{{ $video_url }}">Open
+                                                    YouTube video</a> --}}
                                             @endif
                                         </div>
                                     </div>
                                 </div>
                             </div>
-
-
-                            <!-- Video modal -->
-                            <div class="modal fade" id="bs-video-modal-{{ $cdata->id }}" tabindex="-1" role="dialog"
-                                aria-labelledby="modal-label-{{ $cdata->id }}" aria-hidden="true">
-                                <div class="modal-dialog modal-lg modal-dialog-centered">
-                                    <div class="modal-content">
-                                        <div class="modal-header">
-                                            <button type="button" class="btn-close" data-bs-dismiss="modal"
-                                                aria-hidden="true"></button>
-                                        </div>
-                                        <div class="modal-body pt-0">
-                                            {{-- {{ $cdata->video_url }} --}}
-                                            @php
-                                                $parsed = parse_url($cdata->video_url);
-                                                $checkUrlHost = explode('.', $parsed['host']);
-                                                if (in_array('youtube', $checkUrlHost)) {
-                                                    $video_id = explode('?v=', $cdata->video_url);
-                                                    if (empty($video_id[1])) {
-                                                        $video_id = explode('/v/', $cdata->video_url);
-                                                    }
-                                                    $video_id = explode('&', $video_id[1]);
-                                                    $video_id = $video_id[0];
-                                                    $video_url = 'https://www.youtube.com/embed/' . $video_id;
-                                                } elseif (in_array('vimeo', $checkUrlHost)) {
-                                                    $parse_vimeo = parse_url($cdata->video_url, PHP_URL_PATH);
-                                                    $video_id = array_values(array_filter(explode('/', $parse_vimeo)));
-                                                    $para_vimeo = count($video_id) > 1 ? '?h=' . $video_id[1] : '';
-                                                    $video_url = 'https://player.vimeo.com/video/' . $video_id[0] . $para_vimeo;                                                   
-                                                } else {
-                                                    $video_url = '';
-                                                }
-                                            @endphp
-                                            <iframe src="{{ $video_url }}"
-                                                style="overflow:hidden;height:350px;width: 100%;" frameborder="0"
-                                                allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>
-
-
-                                        </div>
-                                    </div><!-- /.modal-content -->
-                                </div><!-- /.modal-dialog -->
-                            </div><!-- /.modal -->
 
                             <!-- Info modal -->
                             <div class="modal fade" id="bs-info-modal-{{ $cdata->id }}" tabindex="-1" role="dialog"
@@ -101,7 +83,7 @@
                                 <div class="modal-dialog modal-lg modal-dialog-centered">
                                     <div class="modal-content">
                                         <div class="modal-header">
-                                            <h4 class="modal-title" id="modal-label--{{ $cdata->id }}">
+                                            <h4 class="modal-title" id="modal-label-{{ $cdata->id }}">
                                                 {{ $cdata->title }}</h4>
                                             <button type="button" class="btn-close" data-bs-dismiss="modal"
                                                 aria-hidden="true"></button>
@@ -113,6 +95,24 @@
                                 </div><!-- /.modal-dialog -->
                             </div><!-- /.modal -->
                         @endforeach
+
+                        <!-- Video modal -->
+                        <div class="modal fade" id="bs-video-modal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" role="dialog"
+                            aria-labelledby="modal-label" aria-hidden="true">
+                            <div class="modal-dialog modal-lg modal-dialog-centered">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                            aria-hidden="true"></button>
+                                    </div>
+                                    <div class="modal-body pt-0">                                      
+                                        <iframe src="" style="overflow:hidden;height:350px;width: 100%;"
+                                            frameborder="0" allow="autoplay; fullscreen; picture-in-picture"
+                                            allowfullscreen></iframe>
+                                    </div>
+                                </div><!-- /.modal-content -->
+                            </div><!-- /.modal-dialog -->
+                        </div><!-- /.modal -->
                     @else
                         <div class="col-sm-6">
                             <div class="card card-body">
@@ -127,11 +127,18 @@
     </section>
     <!-- /.content -->
 @endsection
+@section('script-section')
+    <script language="javascript" type="text/javascript">
+        $(function() {
+            $('.btn-close').click(function() {
+                $('iframe').attr('src', "");
+            });
 
-<script language="javascript" type="text/javascript">
-    $(function() {
-        $('.btn-close').click(function() {
-            $('iframe').attr('src', $('iframe').attr('src'));
+            $('.play-video').click(function() {
+                var vid_link = $(this).attr('data-video');
+                $('iframe').attr('src', vid_link);
+            });
+
         });
-    });
-</script>
+    </script>
+@endsection
